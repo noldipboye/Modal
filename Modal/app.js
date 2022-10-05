6925ac46e92fd9e3092720a524bc8beef8010c44
Nolan D <38262360+noldipboye@users.noreply.github.com>
2022-10-05 09:33:30 -0500

    Add files via upload

--- Modal/app.js
+++ Modal/app.js
@@ -1,3 +1,12 @@
 function Off() {
-    document.getElementById("Buttons").style.display = "block"
+    document.getElementById("Accept").style.display="block";
+    document.getElementById("Modal-Body").style.display="none";
+    document.getElementById("Decline").style.display="none";
+}
+
+function On() {
+    document.getElementById("Decline").style.display="block";
+    document.getElementById("Modal-Body").style.display="block";
+    document.getElementById("Accept").style.display="none";
+
 }
